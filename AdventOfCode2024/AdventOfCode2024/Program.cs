7aef56c1b263610d9b89d24bffc7b6bcb5eeed9a
Daniel Zürrer <daniel.zuerrer@gmail.com>
2024-12-29 17:37:33 +0100

    day1

---
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Helpers;
+
+var input = await File.ReadAllLinesAsync("Inputs/day1");
+
+var solution = Day1.PuzzleB(input);
+
+Console.WriteLine($"Solution: {solution}");
---
+++ AdventOfCode2024/Solutions/Day1.cs
@@ -0,0 +1,43 @@
+﻿namespace Helpers;
+
+public static class Day1
+{
+    public static int PuzzleA(string[] input)
+    {
+        var (leftList, rightList) = input
+            .Select(line => line.Split("   "))
+            .Aggregate(
+                seed: (new List<int>(), new List<int>()),
+                (agg, locationIds) =>
+                {
+                    agg.Item1.Add(int.Parse(locationIds[0]));
+                    agg.Item2.Add(int.Parse(locationIds[1]));
+                    return agg;
+                });
+
+        var orderedLeftList = leftList.Order().ToList();
+        var orderedRightList = rightList.Order().ToList();
+
+        return orderedLeftList.Zip(orderedRightList, (left, right) => Math.Abs(left - right)).Sum();
+    }
+
+
+   public static int PuzzleB(string[] input)
+    {
+        var (leftList, rightList) = input
+            .Select(line => line.Split("   "))
+            .Aggregate(
+                seed: (new List<int>(), new List<int>()),
+                (agg, locationIds) =>
+                {
+                    agg.Item1.Add(int.Parse(locationIds[0]));
+                    agg.Item2.Add(int.Parse(locationIds[1]));
+                    return agg;
+                });
+
+        var rightCounts = rightList.CountBy(number => number).ToDictionary();
+
+        return leftList
+            .Select(number => rightCounts.GetValueOrDefault(number, 0) * number).Sum();
+    }
+}
